7bc1ba0a7a0887a90aeb432a2ce6f2078fd50627
Akihiro Komori <akihiro.komori@gmail.com>
2018-09-07 15:50:45 +0900

    track the latest TelloLib

--- TelloUnityDemo/Assets/Scripts/TelloController.cs
+++ TelloUnityDemo/Assets/Scripts/TelloController.cs
@@ -14,6 +14,58 @@ public class TelloController : SingletonMonoBehaviour<TelloController> {
 	private byte[] encodedDataPool;
 	private int encodedDataFilled = 0;
 
+			// FlipType is used for the various flips supported by the Tello.
+	public enum FlipType
+	{
+
+		// FlipFront flips forward.
+		FlipFront = 0,
+
+		// FlipLeft flips left.
+		FlipLeft = 1,
+
+		// FlipBack flips backwards.
+		FlipBack = 2,
+
+		// FlipRight flips to the right.
+		FlipRight = 3,
+
+		// FlipForwardLeft flips forwards and to the left.
+		FlipForwardLeft = 4,
+
+		// FlipBackLeft flips backwards and to the left.
+		FlipBackLeft = 5,
+
+		// FlipBackRight flips backwards and to the right.
+		FlipBackRight = 6,
+
+		// FlipForwardRight flips forewards and to the right.
+		FlipForwardRight = 7,
+	};
+
+	// VideoBitRate is used to set the bit rate for the streaming video returned by the Tello.
+	public enum VideoBitRate
+	{
+		// VideoBitRateAuto sets the bitrate for streaming video to auto-adjust.
+		VideoBitRateAuto = 0,
+
+		// VideoBitRate1M sets the bitrate for streaming video to 1 Mb/s.
+		VideoBitRate1M = 1,
+
+		// VideoBitRate15M sets the bitrate for streaming video to 1.5 Mb/s
+		VideoBitRate15M = 2,
+
+		// VideoBitRate2M sets the bitrate for streaming video to 2 Mb/s.
+		VideoBitRate2M = 3,
+
+		// VideoBitRate3M sets the bitrate for streaming video to 3 Mb/s.
+		VideoBitRate3M = 4,
+
+		// VideoBitRate4M sets the bitrate for streaming video to 4 Mb/s.
+		VideoBitRate4M = 5,
+
+	};
+
 	override protected void Awake()
 	{
 		if (!isLoaded) {
@@ -33,12 +85,12 @@ public class TelloController : SingletonMonoBehaviour<TelloController> {
 
 	private void Start()
 	{
-		Tello.start();
+		Tello.startConnecting();
 	}
 
 	void OnApplicationQuit()
 	{
-		Tello.stop();
+		Tello.stopConnecting();
 	}
 
 	// Update is called once per frame
@@ -83,10 +135,10 @@ public class TelloController : SingletonMonoBehaviour<TelloController> {
 
 	}
 
-	private void Tello_onUpdate(Tello.FlyData state)
+	private void Tello_onUpdate(int cmdId)
 	{
 		//throw new System.NotImplementedException();
-		Debug.Log("Tello_onUpdate : " + state);
+		Debug.Log("Tello_onUpdate : " + Tello.state);
 	}
 
 	private void Tello_onConnection(Tello.ConnectionState newState)
@@ -94,19 +146,19 @@ public class TelloController : SingletonMonoBehaviour<TelloController> {
 		//throw new System.NotImplementedException();
 		//Debug.Log("Tello_onConnection : " + newState);
 		if (newState == Tello.ConnectionState.Connected) {
+            Tello.queryAttAngle();
+            Tello.setMaxHeight(50);
+
 			Tello.setPicVidMode(1); // 0: picture, 1: video
-			Tello.setVideoBitRate((int)Tello.VideoBitRate.VideoBitRateAuto);
+			Tello.setVideoBitRate((int)VideoBitRate.VideoBitRateAuto);
 			//Tello.setEV(0);
 			Tello.requestIframe();
 		}
-
 	}
 
-	private void Tello_onVideoData(int fragmentIndex, byte[] data)
+	private void Tello_onVideoData(byte[] data)
 	{
-		if (fragmentIndex == 0)
-			encodedDataFilled = 0;
-		Debug.Log("Tello_onVideoData: " + fragmentIndex);
+		Debug.Log("Tello_onVideoData: " + data.Length);
 		int length = data.Length - 2;
 		Buffer.BlockCopy(data, 2, encodedDataPool, encodedDataFilled, length);
 		encodedDataFilled += length;
