a7440851f12659ee5468be5700e3e464cfc216e3
Akihiro Komori <akihiro.komori@gmail.com>
2025-03-13 17:13:37 +0900

    resolve #13, resolve #14

--- TelloVideoDecoder/TelloVideoDecoder/Android.mk
+++ TelloVideoDecoder/TelloVideoDecoder/Android.mk
@@ -7,21 +7,27 @@ include $(CLEAR_VARS)
 LOCAL_PATH      := $(NDK_PROJECT_PATH)
 LOCAL_MODULE    := libTelloVideoDecoder
 
-FFMPEG_LIB_PATH := $(LOCAL_PATH)/ffmpeg-android-prebuilt/lib
+#FFMPEG_LIB_PATH := $(LOCAL_PATH)/ffmpeg-android-prebuilt-armeabi-v7a/lib
+#LOCAL_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt-armeabi-v7a/include
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt/include
+FFMPEG_LIB_PATH := $(LOCAL_PATH)/ffmpeg-android-prebuilt-arm64-v8a/lib
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/ffmpeg-android-prebuilt-arm64-v8a/include
+
 LOCAL_SRC_FILES := RenderAPI.cpp RenderAPI_OpenGLCoreES.cpp RenderingPlugin.cpp TelloVideoDecoder.cpp
 
-LOCAL_LDFLAGS 	:= -L$(FFMPEG_LIB_PATH)
+LOCAL_LDFLAGS 	:= -L$(FFMPEG_LIB_PATH) -Wl,-z,notext,-Bsymbolic
 
-LOCAL_LDLIBS    := -Wl,--no-warn-shared-textrel \
+# LOCAL_LDLIBS    := -Wl,--no-warn-shared-textrel
+LOCAL_LDLIBS    := -Wl \
 	-llog -lEGL -lGLESv1_CM -lGLESv2 -lz \
-	-lavformat -lavcodec -lswresample -lswscale -lavutil -lx264
+	-lavformat -lavcodec -lswresample -lswscale -lavutil -lx264 -lmp3lame -lvpx
 
 LOCAL_STATIC_LIBRARIES := avcodec avformat avutil swscale x264
 
 APP_ALLOW_MISSING_DEPS=true
 
+LOCAL_CFLAGS += -Wl,-z,notext,-Bsymbolic
+LOCAL_CXXFLAGS += -Wl,-z,notext,-Bsymbolic
 ifeq ($(NDK_DEBUG), 0)
 LOCAL_CFLAGS += -DNDEBUG
 endif
--- TelloVideoDecoder/TelloVideoDecoder/Application.mk
+++ TelloVideoDecoder/TelloVideoDecoder/Application.mk
@@ -1,7 +1,9 @@
 NDK_TOOLCHAIN_VERSION := clang
 APP_OPTIM        := release
-APP_ABI          := armeabi-v7a
-APP_PLATFORM     := android-19
+#APP_ABI          := armeabi-v7a
+APP_ABI          := arm64-v8a
+#APP_PLATFORM     := android-19
+APP_PLATFORM     := android-21
 APP_BUILD_SCRIPT := Android.mk
 APP_STL := c++_static #gnustl_static
-APP_CPPFLAGS += -std=c++11
+APP_CPPFLAGS += -std=c++11 -Wl,-z,notext,-Bsymbolic
